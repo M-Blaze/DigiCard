1ff09edd4500978e8e2d0f824aac8f2e863370f2
Moulik rai <raimoulik@gmail.com>
2024-04-16 20:33:23 +0545

    refactor: remove empty placeholder

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,7 @@ import { register } from '../actions/userActions';
 
 import './register.css'
 
-function RegisterPage(props) {
+function RegisterPage() {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -18,6 +18,7 @@ function RegisterPage(props) {
     const navigate = useNavigate()
     const userRegister = useSelector((state) => state.userRegister)
     const {  error, userInfo } = userRegister
+
     useEffect(() => {
         if (userInfo) {
             navigate('/login')
@@ -26,6 +27,7 @@ function RegisterPage(props) {
 
     const submitHandler = (e) => {
         e.preventDefault()
+        
         if (password !== confirmPassword) {
             setMessage('Passwords do not match')
         } else {
@@ -34,28 +36,28 @@ function RegisterPage(props) {
     }
     return (
         <>  
-            <div class="signup-box">
+            <div className="signup-box" style={{ marginTop:"20px", paddingTop: "20px" }}>
                 <h1 style={{ paddingLeft: 20 }}>Sign Up</h1>
                 {message && <div>{message}</div>}
 
                 {error && <div>{error.errors}</div>}
-                <form onSubmit={submitHandler} className='form'>
+                <form onSubmit={submitHandler} className='form' style={{ marginBottom: "20px" }}>
                     {error && error.name && <div>{error.name}</div>}
                     <label className='label'>First Name</label>
-                    <input onChange={(e) => setName(e.target.value)} value={name} className='input' type="text" placeholder="" />
+                    <input onChange={(e) => setName(e.target.value)} value={name} className='input' type="text" />
                     {error && error.email && <div>{error.email}</div>}
 
                     <label className='label'>Email</label>
-                    <input onChange={(e) => setEmail(e.target.value)} value={email} className='input' type="email" placeholder="" />
+                    <input onChange={(e) => setEmail(e.target.value)} value={email} className='input' type="email" />
                     {error && error.password && <div>{error.password}</div>}
 
                     <label className='label'>Password</label>
-                    <input onChange={(e) => setPassword(e.target.value)} value={password} className='input' type="password" placeholder="" />
+                    <input onChange={(e) => setPassword(e.target.value)} value={password} className='input' type="password" />
                     <label className='label'>Confirm Password</label>
-                    <input onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} className='input' type="password" placeholder="" />
+                    <input onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} className='input' type="password" />
                     <button className='button' type="submit" value="Register">Register</button>
-                    <closeform></closeform></form>
-                <p class="para-2">
+                </form>
+                <p className="para-2">
                     Already have an account? <Link to="/login">Login</Link>
                 </p>
             </div>
