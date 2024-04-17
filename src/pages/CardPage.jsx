28d42ef55824b4ea4816489f8cc2519d024249d0
Moulik rai <raimoulik@gmail.com>
2024-04-17 07:57:24 +0545

    update: remove footer from CardPage

--- src/pages/CardPage.jsx
+++ src/pages/CardPage.jsx
@@ -2,51 +2,44 @@ import React, { useState, useEffect } from 'react';
 import Info from '../components/Info';
 import ContactDetails from '../components/ContactDetails';
 import About from '../components/About';
-import Footer from '../components/Footer';
 import SocialDetails from '../components/SocialDetails';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-function CardPage(props) {
+function CardPage() {
     const { id } = useParams();
-
     const [digiCards, setDigiCards] = useState([]);
     const [loading, setLoading] = useState(true);
 
     const fetchDigiCards = async () => {
         try {
-            const response = await axios.get(` http://192.168.1.14:5000/digiCards/${id}`);
+            const response = await axios.get(`/digiCards/${id}`);
+
             setDigiCards(response.data);
-            setLoading(false); // Set loading to false once data is fetched
-            console.log(response.data)
+            setLoading(false);
         } catch (error) {
             console.error('Error fetching digiCards:', error);
-            setLoading(false); // Set loading to false in case of an error as well
+            setLoading(false);
         }
     };
-    // http://192.168.1.14:3000/digitalcardPage/64d3c1df013d5b3668aceae5
     useEffect(() => {
-        // Fetch digiCards data from the API
-        console.log('hello')
         fetchDigiCards()
-
+        // eslint-disable-next-line
     }, []);
 
-    // Conditional rendering based on loading state
     if (loading) {
-        return <div>Loading...</div>; // You can replace this with a loading spinner or any other UI element
+        return <div>Loading...</div>;
     }
 
     return (
         <div>
-            <div style={{margin:'auto'}} className="cardDesign">
+            <div style={{margin: '30px auto'}} className="cardDesign">
                 <Info selectedFile={digiCards.img} position={digiCards.position} fullName={digiCards.fullName} company={digiCards.company} />
                 <div className="container">
                     <About about={digiCards.about} description={digiCards.description} />
                     <ContactDetails contactDetails={digiCards.contactDetails} />
                     <SocialDetails socialTitle={digiCards.socialTitle} socialDesc={digiCards.socialDesc} socialLinks={digiCards.socialLinks} />
                 </div>
-                <Footer />
             </div>
         </div>
     );
